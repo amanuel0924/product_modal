93ace68ee8835a3834a8b69cbc6d4d6d2ef36b95
amanuel tegegne <amanuel0924@gmail.com>
2023-12-31 19:43:57 +0300

    image added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,15 @@ import headPhone from "./assets/headphone.png"
 function App() {
   return (
     <div className=" flex w-full h-screen justify-center items-center bg-slate-100">
-      <div className=" bg-white shadow-md flex flex-col p-6 m-3 space-y-10 md:space-y-0 md:space-x-10 md:flex-row rounded-lg"></div>
+      <div className=" bg-white shadow-2xl flex flex-col p-6 m-3 space-y-10 md:space-y-0 md:space-x-10 md:flex-row rounded-lg md:m-0 md:p-16">
+        <div>
+          <img
+            src={headPhone}
+            alt="headphone"
+            className=" w-60 mx-auto duration-200  hover:scale-105"
+          />
+        </div>
+      </div>
     </div>
   )
 }
