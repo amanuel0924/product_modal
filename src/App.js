83f6113e6112045120396f684b0b1386576a8c32
amanuel tegegne <amanuel0924@gmail.com>
2023-12-31 19:35:30 +0300

    finish container of modal

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import headPhone from "./assets/headphone.png"
 function App() {
-  return <div></div>
+  return (
+    <div className=" flex w-full h-screen justify-center items-center bg-slate-100">
+      <div className=" bg-white shadow-md flex flex-col p-6 m-3 space-y-10 md:space-y-0 md:space-x-10 md:flex-row rounded-lg"></div>
+    </div>
+  )
 }
 
 export default App
